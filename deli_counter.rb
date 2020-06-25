8d248f99e2d7064f7e00ab8fa0d1e9bcca040def
nickd <nickdiaz58@gmail.com>
2020-06-25 13:36:33 -0400

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+def line(queue)
+    if queue.length == 0
+        puts "The line is currently empty."
+    else
+        string = "The line is currently:"
+        queue.each_with_index do |guest, i|
+            string << " #{i + 1}. #{guest}"
+        end
+        puts string
+    end
+end
+
+def take_a_number(current_line, name)
+    current_line.push(name)
+    puts "Welcome, #{name}. You are number #{current_line.length} in line."
+end
+
+def now_serving(current_line)
+    if current_line.length == 0
+        puts "There is nobody waiting to be served!"
+    else
+        first = current_line.shift
+        puts "Currently serving #{first}."
+    end
+end
