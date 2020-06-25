be72e6cfe1c14e985793e6f5ba259ef1425c8d69
Jessie Donovan <donovanajessie@gmail.com>
2020-06-25 00:54:43 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,14 +1,14 @@
 katz_deli = []
 def line(katz_deli)
   queue = []
-if katz_deli.empty?
-  puts "The line is currently empty."
-else
+  statement= "The line is currently: "
+if katz_deli.length >= 1
 katz_deli.each_with_index do |name, index|
   queue << name
 
-puts "The line is currently: #{index + 1}. #{queue.join(" , ")}"
-end
+puts "#{statement} #{index + 1}. #{queue.join(" , ")}"
+else
+puts "The line is currently empty."
 end
 end
 
